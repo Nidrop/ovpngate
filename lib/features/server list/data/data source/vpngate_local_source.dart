3ab505f19e96ce25280915bd6a8d621946e599e1
Nidrop <aaq7for@gmail.com>
2024-03-02 20:20:07 +0300

    fix caching

--- lib/features/server list/data/data source/vpngate_local_source.dart
+++ lib/features/server list/data/data source/vpngate_local_source.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ovpngate/features/server%20list/data/dto/server_list_mapper.dart';
 import 'package:ovpngate/core/utils/app_paths.dart';
@@ -8,6 +9,8 @@ import 'package:ovpngate/features/server%20list/data/dto/server_info_dto.dart';
 class VpngateLocalSource {
   final String relativeServerListPath = '/vpngate.csv';
   final int rowLength = 15;
+  //TODO make this modifiable in settings
+  final int daysBetweenUpdate = 1;
 
   Future<List<ServerInfoDto>> getServerList({String? relativePath}) async {
     return ServerListMapper.stringToListServerInfoDto(
@@ -18,14 +21,23 @@ class VpngateLocalSource {
   //TODO check date
   Future<bool> isFileRelevant({String? relativePath}) async {
     relativePath ??= relativeServerListPath;
-
     final path = (await AppPaths.getCacheDirPath()) + relativePath;
+
+    int daysBetween(DateTime from, DateTime to) {
+      from = DateTime(from.year, from.month, from.day);
+      to = DateTime(to.year, to.month, to.day);
+      return (to.difference(from).inHours / 24).round();
+    }
+
     final f = File(path);
     if (await f.exists()) {
-      return true;
-    } else {
-      return false;
+      //TODO save modification date in file
+      DateTime fileModDate = (await f.stat()).changed;
+      if (daysBetween(fileModDate, DateTime.now()) <= daysBetweenUpdate) {
+        return true;
+      }
     }
+    return false;
   }
 
   Future<File> saveFile({
