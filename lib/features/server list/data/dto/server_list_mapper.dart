7892525cea03dc2d6e52d7662de8da1baaf7518c
Nidrop <aaq7for@gmail.com>
2024-03-02 20:23:51 +0300

    add conversion error handling

--- lib/features/server list/data/dto/server_list_mapper.dart
+++ lib/features/server list/data/dto/server_list_mapper.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:csv/csv.dart';
+import 'package:flutter/foundation.dart';
 import 'package:ovpngate/core/domain/entity/server_info.dart';
 import 'package:ovpngate/features/server%20list/data/dto/server_info_dto.dart';
 
@@ -12,30 +13,35 @@ sealed class ServerListMapper {
 
     List<ServerInfoDto> list = [];
     final csvList = const CsvToListConverter().convert(rawCSV);
-    for (int i = 2; i < csvList.length; i++) {
+    for (int i = 0; i < csvList.length; i++) {
       final row = csvList[i];
       if (row.length != rowLength) {
         continue;
       }
-      list.add(
-        ServerInfoDto(
-          hostName: row[0] as String,
-          ip: row[1] as String,
-          score: row[2] as int,
-          ping: int.tryParse(row[3].toString()) ?? -1,
-          speed: row[4] as int,
-          countryLong: row[5] as String,
-          countryShort: row[6] as String,
-          numVpnSessions: row[7] as int,
-          uptime: row[8] as int,
-          totalUsers: row[9] as int,
-          totalTraffic: row[10] as int,
-          logType: row[11] as String,
-          operator: row[12] as String,
-          message: row[13] as String,
-          openVPNConfigDataBase64: row[14] as String,
-        ),
-      );
+      try {
+        list.add(
+          ServerInfoDto(
+            hostName: row[0] as String,
+            ip: row[1] as String,
+            score: row[2] as int,
+            ping: int.tryParse(row[3].toString()) ?? -1,
+            speed: row[4] as int,
+            countryLong: row[5] as String,
+            countryShort: row[6] as String,
+            numVpnSessions: row[7] as int,
+            uptime: row[8] as int,
+            totalUsers: row[9] as int,
+            totalTraffic: row[10] as int,
+            logType: row[11] as String,
+            operator: row[12] as String,
+            message: row[13] as String,
+            openVPNConfigDataBase64: row[14] as String,
+          ),
+        );
+      } catch (e) {
+        debugPrint('row $i is not parsed');
+        continue;
+      }
     }
     return list;
   }
