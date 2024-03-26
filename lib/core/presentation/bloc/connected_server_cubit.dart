b411606dd99b55fbd05abd63e773c2d93ca76621
Nidrop <aaq7for@gmail.com>
2024-03-26 17:24:20 +0300

    partial state recovery after closing

--- lib/core/presentation/bloc/connected_server_cubit.dart
+++ lib/core/presentation/bloc/connected_server_cubit.dart
@@ -1,34 +1,59 @@
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:openvpn_flutter/openvpn_flutter.dart';
 import 'package:ovpngate/core/domain/entity/server_info.dart';
 import 'package:ovpngate/core/presentation/bloc/connected_server_state.dart';
+import 'package:ovpngate/core/utils/app_paths.dart';
 
 class ConnectedServerCubit extends Cubit<ConnectedServerState> {
+  //TODO simplify constructor
   ConnectedServerCubit()
       : super(
           ConnectedServerState(
-            vpnstage: VPNStage.disconnected,
+            vpnstage: VPNStage.unknown,
             configCipherFix: true,
             openvpn: OpenVPN(),
           ),
+        ) {
+    final OpenVPN openVPN = OpenVPN(
+        onVpnStatusChanged: _onVpnStatusChanged,
+        onVpnStageChanged: _onVpnStageChanged);
+    openVPN.initialize(
+      // groupIdentifier: 'vpngate.net',
+      // providerBundleIdentifier: 'net.vpn.ovpngate',
+      localizedDescription: 'oVPNGate',
+    );
+    openVPN.stage().then(
+          (stage) => _lastConnected().then(
+            (server) => emit(
+              ConnectedServerState(
+                server: (stage != VPNStage.disconnected) ? server : null,
+                openvpn: openVPN,
+                vpnstage: stage,
+                configCipherFix: true,
+              ),
+            ),
+          ),
         );
-
-  OpenVPN _factoryCreateOpenvpn() {
-    if (state.openvpn.onVpnStageChanged == null ||
-        state.openvpn.onVpnStatusChanged == null) {
-      OpenVPN openVPN = OpenVPN(
-          onVpnStatusChanged: _onVpnStatusChanged,
-          onVpnStageChanged: _onVpnStageChanged);
-      openVPN.initialize(
-        // groupIdentifier: 'vpngate.net',
-        // providerBundleIdentifier: 'net.vpn.ovpngate',
-        localizedDescription: 'oVPNGate',
-      );
-      return openVPN;
-    }
-    return state.openvpn;
   }
+
+  // OpenVPN _factoryCreateOpenvpn() {
+  //   if (state.openvpn.onVpnStageChanged == null ||
+  //       state.openvpn.onVpnStatusChanged == null) {
+  //     final OpenVPN openVPN = OpenVPN(
+  //         onVpnStatusChanged: _onVpnStatusChanged,
+  //         onVpnStageChanged: _onVpnStageChanged);
+  //     openVPN.initialize(
+  //       // groupIdentifier: 'vpngate.net',
+  //       // providerBundleIdentifier: 'net.vpn.ovpngate',
+  //       localizedDescription: 'oVPNGate',
+  //     );
+  //     return openVPN;
+  //   }
+  //   return state.openvpn;
+  // }
 
   void _onVpnStatusChanged(VpnStatus? vpnStatus) {}
 
@@ -36,10 +61,33 @@ class ConnectedServerCubit extends Cubit<ConnectedServerState> {
     debugPrint('DEBUG: $rawStage');
     emit(ConnectedServerState(
       server: state.server,
-      openvpn: _factoryCreateOpenvpn(),
+      openvpn: state.openvpn,
       vpnstage: stage,
       configCipherFix: state.configCipherFix,
     ));
+  }
+
+  //TODO find alternative way to get connected name
+  Future<ServerInfo?> _lastConnected({String? name}) async {
+    final path = '${await AppPaths.getCacheDirPath()}/lastConnectedName.txt';
+    final f = File(path);
+    if (name != null) {
+      await f.writeAsString(name);
+      return null;
+    } else {
+      if (await f.exists()) {
+        String loadedName = await f.readAsString();
+        return ServerInfo(
+            speed: -1,
+            countryShort: "none",
+            sessions: -1,
+            uptime: -1,
+            name: loadedName,
+            ovpnConfig: "none");
+      } else {
+        return null;
+      }
+    }
   }
 
   String _configPatches(String ovpnConfig) {
@@ -47,7 +95,6 @@ class ConnectedServerCubit extends Cubit<ConnectedServerState> {
       //TODO: refactoring
       final result = ovpnConfig.replaceAll(RegExp(r'cipher '), 'data-ciphers ');
       debugPrint('PATCHED');
-      debugPrint(result);
       return result;
     }
     return ovpnConfig;
@@ -56,7 +103,7 @@ class ConnectedServerCubit extends Cubit<ConnectedServerState> {
   void setConfigCipherFix(bool value) {
     emit(ConnectedServerState(
       server: state.server,
-      openvpn: _factoryCreateOpenvpn(),
+      openvpn: state.openvpn,
       vpnstage: state.vpnstage,
       configCipherFix: value,
     ));
@@ -65,7 +112,7 @@ class ConnectedServerCubit extends Cubit<ConnectedServerState> {
   void setServer(ServerInfo server) {
     emit(ConnectedServerState(
       server: server,
-      openvpn: _factoryCreateOpenvpn(),
+      openvpn: state.openvpn,
       vpnstage: state.vpnstage,
       configCipherFix: state.configCipherFix,
     ));
@@ -85,6 +132,7 @@ class ConnectedServerCubit extends Cubit<ConnectedServerState> {
         // bypassPackages: [],
         certIsRequired: true,
       );
+      _lastConnected(name: state.server!.name);
     }
   }
 
