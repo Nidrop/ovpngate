a09df3d73907ffe105b7e4c02cdb95f13782d876
Nidrop <aaq7for@gmail.com>
2024-06-25 09:59:40 +0300

    template settings screen

--- core/lib/constants/api_constants.dart
+++ core/lib/constants/api_constants.dart
@@ -1,4 +1,13 @@
-class ApiConstants {
+abstract class ApiConstants {
   ///Endpoint path
   static const String serverList = '/api/iphone';
+  static const List<String> staticMirrorList = [
+    'https://www.vpngate.net/',
+    'http://109.111.243.206:17579/en/', // (Mirror location: Serbia)
+    'http://78.142.193.246:33304/en/', // (Mirror location: Germany)
+    'http://160.251.62.107:46080/en/', // (Mirror location: Japan)
+    'http://93.185.167.35:54754/en/', // (Mirror location: Moldova Republic of)
+    'http://45.137.155.150:43756/en/', // (Mirror location: United Kingdom)
+    'http://116.80.73.120:23392/en/', // (Mirror location: Japan)
+  ];
 }
--- feature/settings/lib/settings_screen.dart
+++ feature/settings/lib/settings_screen.dart
@@ -13,10 +13,101 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(context.tr(LocaleKeys.common_settings)),
       ),
-      body: ListView(
+      body: Column(
         children: [
-          Text('element'),
+          Text('Theme'),
+          SizedBox(
+            height: 50,
+            child: ThemeToggle(),
+          ),
+          Text('Fetch'),
+          SizedBox(
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('CSV'),
+                Switch(value: false, onChanged: (val) {}),
+                Text('HTML'),
+              ],
+            ),
+          ),
+          Text('Mirrors order'),
+          Flexible(
+            child: MirrorListView(),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class ThemeToggle extends StatefulWidget {
+  const ThemeToggle({
+    super.key,
+  });
+
+  @override
+  State<ThemeToggle> createState() => _ThemeToggleState();
+}
+
+class _ThemeToggleState extends State<ThemeToggle> {
+  final _isSelected = [false, false, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return ToggleButtons(
+      isSelected: _isSelected,
+      onPressed: (int index) {
+        setState(() {
+          for (int buttonIndex = 0;
+              buttonIndex < _isSelected.length;
+              buttonIndex++) {
+            if (buttonIndex == index) {
+              _isSelected[buttonIndex] = true;
+            } else {
+              _isSelected[buttonIndex] = false;
+            }
+          }
+        });
+      },
+      children: const [
+        Icon(Icons.phone_android),
+        Icon(Icons.light_mode),
+        Icon(Icons.dark_mode),
+      ],
+    );
+  }
+}
+
+class MirrorListView extends StatefulWidget {
+  const MirrorListView({
+    super.key,
+  });
+
+  @override
+  State<MirrorListView> createState() => _MirrorListViewState();
+}
+
+class _MirrorListViewState extends State<MirrorListView> {
+  final List<String> _list = [...ApiConstants.staticMirrorList];
+
+  @override
+  Widget build(BuildContext context) {
+    return ReorderableListView.builder(
+      onReorder: (int oldIndex, int newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final String item = _list.removeAt(oldIndex);
+          _list.insert(newIndex, item);
+        });
+      },
+      itemCount: ApiConstants.staticMirrorList.length,
+      itemBuilder: (BuildContext context, int index) => ListTile(
+        key: ValueKey(index),
+        title: Text(_list[index]),
       ),
     );
   }
